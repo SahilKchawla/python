e66a298ac78df11bd939b90f932a736abff4d814
sahil chawla <65318436+SahilKchawla@users.noreply.github.com>
2021-02-23 21:42:18 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,13 @@
+'Write a program to create a tuple and carry some basics operation on it'
+a=('1','2','3','sahil')
+print(a)
+print(type(a))
+
+# a.append('a')
+# a.remove('1')
+
+b=len(a)
+print(b)
+
+print(a[2:])
+print(a[-2:])
