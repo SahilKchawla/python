79a4739eab33aee22dbfadd3e31e4a7394e03177
sahil chawla <65318436+SahilKchawla@users.noreply.github.com>
2021-02-23 21:22:41 +0530

    Add files via upload

---
+++ Gender.py
@@ -0,0 +1,9 @@
+'Write a program to accept gender grom the user and print it'
+
+Gender=input('emter gender in only M and F')
+if Gender == "M":
+    print('person is male')
+elif Gender == "F":
+    print('person is female')
+else:
+    print('error data')
